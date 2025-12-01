a32f1a53c04018b63eb5ab55aae9909339dd63ad
ln_ali <ln@ali.com>
2025-12-01 11:14:40 +0800

    解决 config.py 冲突

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 class Config():
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql://root:zaq12wsx@127.0.0.1:3306/tally_py"
+    SQLALCHEMY_DATABASE_URI = "mysql://root:zaq!@wsx@127.0.0.1:3306/tally_py"
     # 禁用修改追踪以提高性能
     SQLALCHEMY_TRACK_MODIFCATIONS = False
     SECRET_KEY = ""
